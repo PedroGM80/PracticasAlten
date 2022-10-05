1c7acb35851489dee5b90bebd4180bd532155032
PedroGM80 <pedro13087@gmail.com>
2022-10-05 14:44:38 +0200

    add intent

--- app/src/main/java/dev/pgm/praactica321/ActivityA.kt
+++ app/src/main/java/dev/pgm/praactica321/ActivityA.kt
@@ -1,11 +1,29 @@
 package dev.pgm.praactica321
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+
+
+const val EXTRA_MESSAGE = "twoactivities.extra.MESSAGE"
 
 class ActivityA : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_a)
+
+        val button: Button = findViewById(R.id.activityBtnLaunchIntent)
+
+        button.setOnClickListener {
+
+            val intent = Intent(this, ActivityB::class.java)
+            val editTextMsn: EditText = findViewById(R.id.activityEditTextTextTop)
+            val message: String = editTextMsn.text.toString()
+
+            intent.putExtra(EXTRA_MESSAGE, message)
+            startActivity(intent)
+        }
     }
 }
