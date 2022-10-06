02239ebae1aa447a8c645019330bde999c1a961f
PedroGM80 <pedro13087@gmail.com>
2022-10-06 13:28:58 +0200

    changeColor

--- app/src/main/java/dev/pgm/praactica321/ActivityA.kt
+++ app/src/main/java/dev/pgm/praactica321/ActivityA.kt
@@ -1,56 +1,65 @@
 package dev.pgm.praactica321
 
 import android.content.Intent
+import android.graphics.Color
 import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 
 
 class ActivityA : AppCompatActivity() {
-    // private val LOG_TAG: String = ActivityA::class.java.simpleName
-    val TEXT_REQUEST = 1
 
-    // Unique tag required for the intent extra
+    private val request = 1
+
     companion object {
-        const val EXTRA_MESSAGE = "com.example.android.twoactivities.extra.MESSAGE"
+        const val EXTRA_MESSAGE = "Message"
+        const val EXTRA_COLOR_VALUE = "Color"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_a)
 
+        if (intent.hasExtra(EXTRA_COLOR_VALUE)) {
+            val colorValue = intent.getStringExtra(EXTRA_COLOR_VALUE).toString()
+            val constraintLayout = findViewById<ConstraintLayout>(R.id.activityAConstraintInitialView)
+            constraintLayout.setBackgroundColor(Color.parseColor(colorValue))
+        }
         val buttonLaunchToast: Button = findViewById(R.id.activityBtnLaunchIntent)
         val buttonLaunchResult: Button = findViewById(R.id.activityBtnLaunchIntentResult)
-        val buttonWebOpen:Button=findViewById(R.id.activityBtnLaunchIntentWeb)
+        val buttonWebOpen: Button = findViewById(R.id.activityBtnLaunchIntentWeb)
+
         buttonLaunchToast.setOnClickListener {
 
             val intent = Intent(this, ActivityB::class.java)
-            val editTextMsn: EditText = findViewById(R.id.activityEditTextTextTop)
+            val editTextMsn: EditText = findViewById(R.id.activityAEditTextMessageUserIn)
             val message: String = editTextMsn.text.toString()
 
             intent.putExtra(EXTRA_MESSAGE, message)
             startActivity(intent)
-            //  startActivityForResult(intent, TEXT_REQUEST)
+
         }
 
         buttonLaunchResult.setOnClickListener {
 
             val intent = Intent(this, ActivityB::class.java)
-            val editTextMsn: EditText = findViewById(R.id.activityEditTextTextTop)
+            val editTextMsn: EditText = findViewById(R.id.activityAEditTextMessageUserIn)
             val message: String = editTextMsn.text.toString()
 
             intent.putExtra(EXTRA_MESSAGE, message)
-
-            startActivityForResult(intent, TEXT_REQUEST)
+            startActivityForResult(intent, request)
         }
 
-        buttonWebOpen.setOnClickListener{
+        buttonWebOpen.setOnClickListener {
+
             val url = "http://www.google.com"
-            val i = Intent(Intent.ACTION_VIEW)
-            i.data = Uri.parse(url)
-            startActivity(i)
+            val webOpenIntent = Intent(Intent.ACTION_VIEW)
+
+            webOpenIntent.data = Uri.parse(url)
+            startActivity(webOpenIntent)
         }
     }
 }
--- app/src/main/java/dev/pgm/praactica321/ActivityB.kt
+++ app/src/main/java/dev/pgm/praactica321/ActivityB.kt
@@ -2,19 +2,49 @@ package dev.pgm.praactica321
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import dev.pgm.praactica321.ActivityA.Companion.EXTRA_COLOR_VALUE
 import dev.pgm.praactica321.ActivityA.Companion.EXTRA_MESSAGE
 
 
 class ActivityB : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_b)
 
-        val intent: Intent = intent
-        val message = intent.getStringExtra(EXTRA_MESSAGE)
 
-        if (message?.isNotBlank() == true) Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+        val message = intent.getStringExtra(EXTRA_MESSAGE)
+        if (message?.isNotBlank() == true) {
+
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+        }
+
+        val buttonRed: Button = findViewById(R.id.activityBtnBackColorRed)
+        val buttonGreen: Button = findViewById(R.id.activityBtnBackColorGreen)
+        val buttonBlue: Button = findViewById(R.id.activityBtnBackColorBlue)
+
+        buttonRed.setOnClickListener {
+
+            val intentColor = Intent(this, ActivityA::class.java)
+            intentColor.putExtra(EXTRA_COLOR_VALUE, R.color.red)
+            startActivity(intentColor)
+        }
+
+        buttonGreen.setOnClickListener {
+
+            val intentColor = Intent(this, ActivityA::class.java)
+            intentColor.putExtra(EXTRA_COLOR_VALUE, R.color.green.toString())
+            startActivity(intentColor)
+        }
+        buttonBlue.setOnClickListener {
+
+            val intentColor = Intent(this, ActivityA::class.java)
+            intentColor.putExtra(EXTRA_COLOR_VALUE, R.color.blue.toString())
+            startActivity(intentColor)
+        }
     }
 }
